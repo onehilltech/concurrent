8757ccf04dd154ea458a50a8404581d2bbb86eae
James H. Hill <hilljh82@gmail.com>
2016-07-15 19:30:07 -0400

    Implemented proxy object for CompletionCallback

---
+++ src/main/java/com/onehilltech/concurrent/CompletionCallbackProxy.java
@@ -0,0 +1,36 @@
+package com.onehilltech.concurrent;
+
+/**
+ * Proxy class that allows you to alter the behavior of an existing CompletionCallback
+ * object. Any method not overloaded is passed through to the original CompletionCallback
+ * object.
+ *
+ * The proxy also has the option of passing control to the original object.
+ */
+public class CompletionCallbackProxy implements CompletionCallback
+{
+  private final CompletionCallback callback_;
+
+  public CompletionCallbackProxy (CompletionCallback callback)
+  {
+    this.callback_ = callback;
+  }
+
+  @Override
+  public void onCancel ()
+  {
+    this.callback_.onCancel ();
+  }
+
+  @Override
+  public void onFail (Exception e)
+  {
+    this.callback_.onFail (e);
+  }
+
+  @Override
+  public void onComplete (Object o)
+  {
+    this.callback_.onComplete (o);
+  }
+}
