6dac7d381880722c6bbbe216f88e696ac7de3e4a
James H. Hill <hilljh82@gmail.com>
2016-08-15 00:29:54 -0400

    Implemented done() with no result; added more documentation

--- lib/src/main/java/com/onehilltech/concurrent/CompletionCallback.java
+++ lib/src/main/java/com/onehilltech/concurrent/CompletionCallback.java
@@ -1,18 +1,56 @@
 package com.onehilltech.concurrent;
 
+/**
+ * @class CompletionCallback
+ *
+ * Callback used to signal completion of a task.
+ */
 public abstract class CompletionCallback
 {
-  public final void done (Object result)
+  /**
+   * The task is done with no result.
+   */
+  public void done ()
+  {
+    this.done (null);
+  }
+
+  /**
+   * The task is done and has a result.
+   *
+   * @param result        Result of the task
+   */
+  public void done (Object result)
   {
     this.onComplete (result);
   }
 
-  public final void fail (Throwable e)
+  /**
+   * The task failed.
+   *
+   * @param e             Reason for failure
+   */
+  public void fail (Throwable e)
   {
     this.onFail (e);
   }
 
+  /**
+   * Handle notification that the task has failed.
+   *
+   * @param e
+   */
   protected abstract void onFail (Throwable e);
+
+  /**
+   * Handle notification that the task, or tasks, was cancelled.
+   */
   protected abstract void onCancel ();
+
+  /**
+   * Handle notification that the task was completed.
+   *
+   * @param result
+   */
   protected abstract void onComplete (Object result);
 }
